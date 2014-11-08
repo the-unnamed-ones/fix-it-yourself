1a6921921413c000bd147aad5825ad5573e2800d
bloodcount <searosss@gmail.com>
2014-11-08 20:04:38 +0200

    added the communication script

---
+++ server_communication.js
@@ -0,0 +1,12 @@
+
+function getFixItJson(params){
+ $.ajax({
+      type: "GET",
+      url: "http://192.168.1.100/fixit-api/api.pl",
+       data: {payload_json: JSON.stringify(params)},
+       success: function(msg){
+         alert(msg);
+       }
+      });
+
+}
